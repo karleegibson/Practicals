3f98dfb95105e2a0633c68ed24bf5e6f7c81dd96
karlee.gibson <karlee.gibson@my.jcu.edu.au>
2016-08-18 12:53:55 +1000

    Create function for fixedScore

--- Prac03/fixedScore.py
+++ Prac03/fixedScore.py
@@ -1,9 +1,18 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif 50 < score <= 90:
-    print("Passable")
-elif score > 90:
-    print("Excellent")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    valid_score_message = check_valid_score(score)
+    print(valid_score_message)
+
+def check_valid_score(score):
+    if score < 0 or score > 100:
+        message = "Invalid score"
+    elif 50 < score <= 90:
+        message = "Passable"
+    elif score > 90:
+        message = "Excellent"
+    else:
+        message = "Bad"
+    return message
+
+
+main()
