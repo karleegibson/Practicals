e63173acb715376da5b89be268b8f282bdab3205
karlee.gibson <karlee.gibson@my.jcu.edu.au>
2016-08-18 12:47:04 +1000

    Create two functions for temperatures

--- Prac03/temperatures.py
+++ Prac03/temperatures.py
@@ -7,15 +7,27 @@ __author__ = 'Lindsay Ward'
 MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def convert_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def convert_to_celsius():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
 while choice != "Q":
     if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
+        celsius_to_fahrenheit = convert_to_fahrenheit()
+        print("Result: {:.2f} F".format(celsius_to_fahrenheit))
     elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
+        fahrenheit_to_celsius = convert_to_celsius()
+        print("Result: {:.2f} C".format(fahrenheit_to_celsius))
     else:
         print("Invalid option")
     print(MENU)
