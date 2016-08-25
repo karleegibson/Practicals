e30f2053c82af59b2afcb2b5cfbb0b258161898b
karlee.gibson@my.jcu.edu.au <Eelrak99>
2016-08-25 12:36:57 +1000

    Initialise securityChecker. Add username conditions to grant or deny access depending on username matchup with list of usernames.

---
+++ Prac04/securityChecker.py
@@ -0,0 +1,9 @@
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    user_name = input("Enter username: ")
+    if user_name in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+main()
