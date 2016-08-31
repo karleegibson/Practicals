112f3e561b024813ad12412f0662d42efc8f96af
karleegibson <karlee.gibson@my.jcu.edu.au>
2016-08-31 16:06:09 +1000

    Complete 2 TODOs for comprehensionsList

--- Prac04/comprehensionsList.py
+++ Prac04/comprehensionsList.py
@@ -25,8 +25,9 @@ print(full_initials)
 almost_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # TODO: use a list comprehension to create a list of integers from this list of strings
-numbers = [[int(y) for y in x] for x in almost_numbers]
+numbers = [(int(i)) for i in almost_numbers]
 print(numbers)
 
 # TODO: use a list comprehension to create a list of all of the full_names in lowercase
-# lowercase_full_names =
+lowercase_full_names = [(name.lower()) for name in full_names]
+print(lowercase_full_names)
