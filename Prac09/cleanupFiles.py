71e4e3ba1252fd9a974a0019995712106d13981b
karleegibson <karlee.gibson@my.jcu.edu.au>
2016-10-13 11:34:14 +1000

    Edit code for cleanupFiles. Create function to get fixed file name

--- Prac09/cleanupFiles.py
+++ Prac09/cleanupFiles.py
@@ -6,37 +6,49 @@ import os, shutil
 
 __author__ = 'Lindsay Ward'
 
-print("Current directory is", os.getcwd())
 
-# change to desired directory
-os.chdir('Lyrics/Christmas')
-# print a list of all files (test)
-# print(os.listdir('.'))
+def main():
+    print("Current directory is", os.getcwd())
 
-# make a new directory
-# os.mkdir('temp')
+    # change to desired directory
+    os.chdir('Lyrics/Christmas')
+    # print a list of all files (test)
+    # print(os.listdir('.'))
 
-# loop through each file in the (original) directory
-for filename in os.listdir('.'):
-    # ignore directories, just process files
-    if not os.path.isdir(filename):
-        new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-        print(new_name)
+    # make a new directory
+    # os.mkdir('temp')
 
-        # Option 1: rename file to new name - in place
-        os.rename(filename, new_name)
+    # loop through each file in the (original) directory
+    # for filename in os.listdir('.'):
+        # ignore directories, just process files
+        # if not os.path.isdir(filename):
+            # new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+            # print(new_name)
 
-
-        # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
-
-names = []
-if
-
-print(names)
+            # Option 1: rename file to new name - in place
+            # os.rename(filename, new_name)
 
 
+            # Option 2: move file to new place, with new name
+            # shutil.move(filename, 'temp/' + new_name)
 
+    for filename in os.listdir('.'):
+        fixed_filename = get_fixed_filename(filename)
+        print(fixed_filename)
+
+
+def get_fixed_filename(filename):
+    newstring = filename[0] + ''
+    for char in filename[1:]:
+        if char.isupper():
+            newstring += "_" + char
+        else:
+            newstring += char
+    return newstring
+
+# separate variable or enumerate to keep track of char number. Char before cannot be an underscore
+
+main()
 
 # Processing subdirectories using os.walk()
 # os.chdir('..')
